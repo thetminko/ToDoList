718bbf4ffcdade9e52e0cdca7f13404db02ad862
thetminko <minko.thet@ufinity.com>
2018-08-30 18:38:29 +0800

    Trying out the filter authorization

--- server/src/server.js
+++ server/src/server.js
@@ -1,40 +1,10 @@
-import server from './routes';
+import server from './routes/routes';
 
 const APP_CONFIG = {
     "cors-allow-origin": "*",
     "cors-allow-headers": "Origin, X-Requested-With, Content-Type, Accept",
     "port": 2000
 }
-
-/* ----------------------- GLOBAL HANDLERS ------------------------- */
-
-/*
-To handle 404
-*/
-server.use((req, res, next) => {
-    let error = new Error("Not Found!");
-    error.status = 404;
-    next(error);
-});
-
-/*
-To handle errors
-*/
-server.use((err, req, res, next) => {
-
-    if (!err.status) {
-        err.status = 500;
-    }
-
-    if (!err.message) {
-        err.message = "Something went wrong!";
-    }
-
-    res.status(err.status).json({
-        message: err.message
-    });
-});
-
 
 
 /* ----------------------- SERVER START UP CONFIG ------------------------- */
