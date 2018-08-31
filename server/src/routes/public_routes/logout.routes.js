7f7ddc25873e996be7c8bdd658da8271b5b4e565
thetminko <minko.thet@ufinity.com>
2018-08-31 17:41:44 +0800

    Refactored to remove files as it is small function

--- server/src/routes/public_routes/logout.routes.js
+++ server/src/routes/public_routes/logout.routes.js
@@ -6,7 +6,6 @@ const logoutRouter = Router();
 logoutRouter.get("/", (req, res) => {
     res.cookie('authenticated', false);
     res.json({message: "Logged out!"});
-    logger().log(req.cookies);
 });
 
 export default logoutRouter;
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,9 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import loginRouter from './routes/public_routes/login.routes';
-import protectedResourceFilter from './filters/protected.resource.filter';
 import toDoRouter from './routes/protected_routes/todolist.routes';
-import requestLogFilter from './filters/request.log.filter';
 import logoutRouter from './routes/public_routes/logout.routes';
-import { logger } from './utils/AppUtils';
+import { logger, isLoggedIn } from './utils/AppUtils';
 
 const SERVER_CONFIG = {
     "cors-allow-origin": "*",
@@ -22,11 +20,14 @@ server.use(cookieParser());
 server.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next(); 
+    next();
 });
 
 // Request logging middleware
-server.use(requestLogFilter);
+server.use("*", (req, res, next) => {
+    logger().log("Requested URL: ", req.originalUrl);
+    next();
+});
 
 
 /* ----------------------- END OF REGISTERING PRE MIDDLEWARES ------------------------- */
@@ -43,8 +44,21 @@ server.use(requestLogFilter);
 server.use("/login", loginRouter);
 server.use("/logout", logoutRouter);
 
+
 // Protected resource filters
-server.use(protectedResourceFilter);
+server.use("*", (req, res, next) => {
+    loginFilter();
+
+    function loginFilter() {
+        if (!isLoggedIn(req.cookies)) {
+            var err = new Error("Unauthorized access!");
+            err.status = 401;
+            next(err);
+        } else {
+            next();
+        }
+    }
+});
 
 // Protected resource routers
 server.use("/", toDoRouter);
@@ -71,7 +85,7 @@ server.use((req, res, next) => {
 // Handle unexpected errors
 server.use((err, req, res, next) => {
     if (err.status === 401) {
-      err.message = "Please login first!";
+        err.message = "Please login first!";
     }
 
     if (!err.status) {
@@ -99,7 +113,7 @@ server.use((err, req, res, next) => {
 /* ----------------------- SERVER START UP CONFIG ------------------------- */
 
 server.listen(SERVER_CONFIG["port"], () => {
-    logger().log("Environment: ", process.env);
+    // logger().log("Environment: ", process.env);
     logger().log("Server has started at port: ", SERVER_CONFIG["port"]);
 });
 
